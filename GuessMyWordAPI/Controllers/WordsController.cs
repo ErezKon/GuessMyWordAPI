3cbfdf12cc6d584b4e6ce8e50d0f25517b0a3b2e
Erez Konforti <Erez.Konforti@Dell.com>
2022-08-16 21:30:13 +0300

    test stub

---
+++ GuessMyWordAPI/Controllers/WordsController.cs
@@ -0,0 +1,35 @@
+﻿using GuessMyWordAPI.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuessMyWordAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordsController : ControllerBase
+    {
+        [HttpGet("GetWord")]
+        public WordVM GetWord(WordVM word)
+        {
+            return word;
+        }
+
+        [HttpGet("GetRandomWord")]
+        public WordVM GetRandomWord(string language)
+        {
+            return new WordVM
+            {
+                ID = Guid.NewGuid(),
+                Language = language,
+                Word = "Test Word"
+            };
+        }
+
+
+        [HttpGet("AddWord")]
+        public WordVM AddWord(WordVM word)
+        {
+            return word;
+        }
+    }
+}
---
+++ GuessMyWordAPI/ViewModels/WordVM.cs
@@ -0,0 +1,9 @@
+﻿namespace GuessMyWordAPI.ViewModels
+{
+    public class WordVM
+    {
+        public Guid ID { get; set; }
+        public string Word { get; set; }
+        public string Language { get; set; }
+    }
+}
